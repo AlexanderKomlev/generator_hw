58f50e463130f4067277c94ef029e16a266c9937
Alexander Komlev <alexander.komlev42@gmail.com>
2024-08-03 19:13:19 +0700

    initial commit

---
+++ src/team.js
@@ -0,0 +1,12 @@
+export default class Team {
+  constructor() {
+    this.members = [];
+  }
+
+  * [Symbol.iterator]() {
+    let index = 0;
+    while (index < this.members.length) {
+      yield this.members[index++];
+    }
+  }
+}
